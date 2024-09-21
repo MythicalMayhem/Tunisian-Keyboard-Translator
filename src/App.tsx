2f139d03cf2a5b61b5a2f0e60372ff2220765e52
MythicalMayhem <ameuroussema@gmail.com>
2024-09-21 20:41:46 +0100

    css first

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/App.css'
+import Input from './components/input'
+import Title from './components/title'
+import Result from './components/result'
+function App() {
+    return (
+        <>
+            <Title />
+            <Input />
+            <Result />
+            <footer></footer>
+        </>);
+}
+
+export default App;
---
+++ src/components/result.tsx
@@ -0,0 +1,11 @@
+function Result() {
+    return (
+        <fieldset className="result">
+            <legend>
+                Result
+            </legend>
+        </fieldset>
+    );
+}
+
+export default Result;
